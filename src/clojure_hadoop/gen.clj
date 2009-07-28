502b53eb1b1bf0e73c25a3621cac2b426e87a2a1
Stuart Sierra <mail@stuartsierra.com>
2009-07-28 16:37:22 -0400

    gen.clj: added doc strings

--- src/clojure_hadoop/gen.clj
+++ src/clojure_hadoop/gen.clj
@@ -1,6 +1,29 @@
-(ns clojure-hadoop.gen)
+(ns #^{:doc "Class-generation helpers for writing Hadoop jobs in Clojure."}
+    clojure-hadoop.gen)
 
-(defmacro gen-job-classes []
+(defmacro gen-job-classes
+  "Creates gen-class forms for Hadoop job classes from the current
+  namespace. Now you only need to write three functions:
+
+  (defn mapper-map [this key value output reporter] ...)
+
+  (defn reducer-reduce [this key values output reporter] ...)
+
+  (defn tool-run [& args] ...)
+
+  The first two functions are the standard map/reduce functions in any
+  Hadoop job.
+
+  The third function, tool-run, will be called by the Hadoop framework
+  to start your job, with the arguments from the command line.  It
+  should set up the JobConf object and call JobClient/runJob, then
+  return zero on success.
+
+  You must also call gen-main-method to create the main method.
+
+  After compiling your namespace, you can run it as a Hadoop job using
+  the standard Hadoop command-line tools."
+  []
   (let [the-name (.replace (str (ns-name *ns*)) \- \_)]
     `(do 
        (gen-class
@@ -20,7 +43,10 @@
         :implements ["org.apache.hadoop.mapred.Reducer"]
         :prefix "reducer-"))))
 
-(defn gen-main-method []
+(defn gen-main-method
+  "Adds a standard main method, named tool-main, to the current
+  namespace."
+  []
   (let [the-name (.replace (str (ns-name *ns*)) \- \_)]
     (intern *ns* 'tool-main
             (fn [& args]
