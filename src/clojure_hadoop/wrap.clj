ae375e859a0c5803e52ea0f95f66b2a9cda49105
Stuart Sierra <mail@stuartsierra.com>
2009-07-28 16:47:30 -0400

    wrap.clj: added doc strings

--- src/clojure_hadoop/wrap.clj
+++ src/clojure_hadoop/wrap.clj
@@ -1,4 +1,6 @@
-(ns clojure-hadoop.wrap
+(ns #^{:doc "Map/Reduce wrappers that set up common input/output
+  conversions for Clojure jobs."}
+  clojure-hadoop.wrap
   (:require [clojure-hadoop.imports :as imp]))
 
 (imp/import-io)
@@ -6,7 +8,21 @@
 
 (declare *reporter*)
 
-(defn wrap-map [f]
+(defn wrap-map
+  "Returns a function implementing the Mapper.map interface.
+
+  The returned function uses read-string to read in Clojure data
+  structures from the key and value (which must be of type
+  org.apache.hadoop.io.Text).  Then it calls (f key value).
+
+  f must return a *sequence* of *pairs* like 
+    [[key1 value1] [key2 value2] ...]
+
+  The pairs returned by f will be serialized to strings with pr-str,
+  then sent to the Hadoop OutputCollector as Text objects.
+
+  When f is called, *reporter* is bound to the Hadoop Reporter."
+  [f]
   (fn [this wkey wvalue output reporter]
     (binding [*reporter* reporter]
       (doseq [[key value] (f (read-string (.toString wkey))
@@ -14,7 +30,21 @@
         (binding [*print-dup* true]
           (.collect output (Text. (pr-str key)) (Text. (pr-str value))))))))
 
-(defn wrap-reduce [f]
+(defn wrap-reduce
+  "Returns a function implementing the Reducer.reduce interface.
+
+  The returned function uses read-string to read in Clojure data
+  structures from the key and values (which must be of type
+  org.apache.hadoop.io.Text).  Then it calls (f key values).
+
+  f must return a *sequence* of *pairs* like 
+    [[key1 value1] [key2 value2] ...]
+
+  The pairs returned by f will be serialized to strings with pr-str,
+  then sent to the Hadoop OutputCollector as Text objects.
+
+  When f is called, *reporter* is bound to the Hadoop Reporter."
+  [f]
   (fn [this wkey wvalues output reporter]
     (binding [*reporter* reporter]
       (doseq [[key value] (f (read-string (.toString wkey))
