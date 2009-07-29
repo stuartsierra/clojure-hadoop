c209259ad90f4614ffd4f7189fd4eb40dfc46b96
Stuart Sierra <mail@stuartsierra.com>
2009-07-29 12:40:54 -0400

    wordcount1.clj: added lots of documentation

--- examples/wordcount1.clj
+++ examples/wordcount1.clj
@@ -1,4 +1,24 @@
-(ns wordcount1
+(ns #^{:doc "wordcount1 -- low-level MapReduce example
+
+  This namespace demonstrates how to use the lower layers of
+  abstraction provided by the clojure-hadoop library.
+
+  This is the example word count program used in the Hadoop MapReduce
+  tutorials.  As you can see, it is very similar to the Java code, and
+  uses the Hadoop API directly.
+
+  We have to call gen-job-classes and gen-main-method, then define the
+  three functions mapper-map, reducer-reduce, and tool-run.
+
+  To run this example, first compile it (see instructions in
+  README.txt), then run this command:
+
+    java -cp examples.jar:lib/* wordcount1 README.txt out1
+
+  This will count the instances of each word in README.txt and write
+  the results to out1/part-00000
+  "}
+  wordcount1
   (:require [clojure-hadoop.gen :as gen]
             [clojure-hadoop.imports :as imp])
   (:import (java.util StringTokenizer)))
@@ -7,18 +27,44 @@
 (imp/import-fs)     ;; for Path
 (imp/import-mapred) ;; for JobConf, JobClient
 
-(gen/gen-job-classes)
-(gen/gen-main-method)
+(gen/gen-job-classes)  ;; generates Tool, Mapper, and Reducer classes
+(gen/gen-main-method)  ;; generates Tool.main method
 
-(defn mapper-map [this key value output reporter]
+(defn mapper-map
+  "This is our implementation of the Mapper.map method.  The key and
+  value arguments are sub-classes of Hadoop's Writable interface, so
+  we have to convert them to strings or some other type before we can
+  use them.  Likewise, we have to call the OutputCollector.collect
+  method with objects that are sub-classes of Writable."
+  [this key value output reporter]
   (doseq [word (enumeration-seq (StringTokenizer. (str value)))]
     (.collect output (Text. word) (IntWritable. 1))))
 
-(defn reducer-reduce [this key values output reporter]
-  (.collect output key
-            (reduce + (map #(.get %) (iterator-seq values)))))
+(defn reducer-reduce 
+  "This is our implementation of the Reducer.reduce method.  The key
+  argument is a sub-class of Hadoop's Writable, but 'values' is a Java
+  Iterator that returns successive values.  We have to use
+  iterator-seq to get a Clojure sequence from the Iterator.  
 
-(defn tool-run [this args]
+  Beware, however, that Hadoop re-uses a single object for every
+  object returned by the Iterator.  So when you get an object from the
+  iterator, you must extract its value (as we do here with the 'get'
+  method) immediately, before accepting the next value from the
+  iterator.  That is, you cannot hang on to past values from the
+  iterator."
+  [this key values output reporter]
+  (let [sum (reduce + (map #(.get %) (iterator-seq values)))]
+    (.collect output key (IntWritable. sum))))
+
+(defn tool-run
+  "This is our implementation of the Tool.run method.  args are the
+  command-line arguments as a Java array of strings.  We have to
+  create a JobConf object, set all the MapReduce job parameters, then
+  call the JobClient.runJob static method on it.
+
+  This method must return zero on success or Hadoop will report that
+  the job failed."
+  [this args]
   (doto (JobConf. (.getConf this) (.getClass this))
     (.setJobName "wordcount1")
     (.setOutputKeyClass Text)
