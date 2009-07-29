cd81add1515ea86d6b608c9cf86ee19ae5c7a888
Stuart Sierra <mail@stuartsierra.com>
2009-07-29 12:02:06 -0400

    wordcount3.clj: only contains reduce function

--- examples/wordcount3.clj
+++ examples/wordcount3.clj
@@ -1,13 +1,6 @@
 (ns wordcount3
-  (:require [clojure-hadoop.wrap :as wrap])
   (:import (java.util StringTokenizer)))
 
-(defn my-map [this key value output reporter]
-  (doseq [word (enumeration-seq (StringTokenizer. (str value)))]
-    (.collect output (Text. (pr-str word)) (Text. (pr-str 1)))))
-
-(def my-reduce
-     (wrap/wrap-reduce
-      (fn [key values]
-        [[key (reduce + values)]])))
+(defn my-reduce [key values]
+  [[key (reduce + values)]])
 
