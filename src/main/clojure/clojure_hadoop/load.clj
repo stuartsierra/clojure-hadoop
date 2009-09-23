011b3137cc43b1055f56bc4142452166816bea97
Stuart Sierra <mail@stuartsierra.com>
2009-09-23 12:32:21 -0400

    load.clj: move load-name from job.clj

---
+++ src/main/clojure/clojure_hadoop/load.clj
@@ -0,0 +1,13 @@
+(ns clojure-hadoop.load)
+
+(defn load-name
+  "Loads and returns the value of a namespace-qualified string naming
+  a symbol.  If the namespace is not currently loaded it will be
+  require'd."
+  [s]
+  (let [[ns-name fn-name] (.split s "/")]
+    (when-not (find-ns (symbol ns-name))
+      (require (symbol ns-name)))
+    (assert (find-ns (symbol ns-name)))
+    (deref (resolve (symbol ns-name fn-name)))))
+
