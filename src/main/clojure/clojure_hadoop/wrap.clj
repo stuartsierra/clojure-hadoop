ba2f3aabe67d6297f47742c34b74568dc48f8a5d
Stuart Sierra <mail@stuartsierra.com>
2009-11-17 22:58:43 -0500

    wrap.clj: create non-caching-iterator-seq for reducer

--- src/main/clojure/clojure_hadoop/wrap.clj
+++ src/main/clojure/clojure_hadoop/wrap.clj
@@ -8,6 +8,13 @@
 
 (declare *reporter*)
 
+(defn- non-caching-iterator-seq [#^java.util.Iterator iter]
+  (proxy [clojure.lang.ISeq] []
+    (seq [] this)
+    (first [] (when (.hasNext iter) (.next iter)))
+    (next [] (when (.hasNext iter) this))
+    (more [] (if (.hasNext iter) this (list)))))
+
 (defn string-map-reader
   "Returns a [key value] pair by calling .toString on the Writable key
   and value."
@@ -28,7 +35,7 @@
   string representations of the Writable key and values."
   [wkey wvalues]
   [(read-string (.toString wkey))
-   (map #(read-string (.toString %)) (iterator-seq wvalues))])
+   (map #(read-string (.toString %)) (non-caching-iterator-seq wvalues))])
 
 (defn clojure-writer
   "Sends key and value to the OutputCollector by calling pr-str on key
